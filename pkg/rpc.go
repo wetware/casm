f715090fc92c2ce595c3fae6aa39337277f20035
Louis Thibault <l.thibault@sentimens.com>
2023-02-23 21:41:35 -0500

    Remove casm.FuturePtr. Broken by design.

--- pkg/rpc.go
+++ pkg/rpc.go
@@ -28,52 +28,6 @@ func (f Future) Await(ctx context.Context) error {
 	return f.Err()
 }
 
-type FuturePtr struct{ *capnp.Future }
-
-func (f FuturePtr) Client() capnp.Client {
-	ptr, err := f.Ptr()
-	if err != nil {
-		return capnp.ErrorClient(err)
-	}
-
-	return ptr.Interface().Client()
-}
-
-func (f FuturePtr) Await(ctx context.Context) (capnp.Ptr, error) {
-	select {
-	case <-f.Done():
-	case <-ctx.Done():
-	}
-
-	// The future may have resolved due to a canceled context, in which
-	// case there is a race-condition in the above select.
-	if ctx.Err() != nil {
-		return capnp.Ptr{}, ctx.Err()
-	}
-
-	return f.Ptr()
-}
-
-func (f FuturePtr) AwaitBytes(ctx context.Context) ([]byte, error) {
-	ptr, err := f.Await(ctx)
-	return ptr.Data(), err
-}
-
-func (f FuturePtr) AwaitString(ctx context.Context) (string, error) {
-	ptr, err := f.Await(ctx)
-	return ptr.Text(), err
-}
-
-func (f FuturePtr) Bytes() ([]byte, error) {
-	ptr, err := f.Ptr()
-	return ptr.Data(), err
-}
-
-func (f FuturePtr) String() (string, error) {
-	ptr, err := f.Ptr()
-	return ptr.Text(), err
-}
-
 type Iterator[T any] struct {
 	Seq interface {
 		Next() (T, bool)
